65b73ce3bc2d76af6b93d0cb78dc894c060e22bc
einnivd <yelk0427@gmail.com>
2026-06-15 16:02:39 +0900

    리액트 시작 - 첫번째 연습

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "../src/App.css"
+function App() {
+  return(
+    <>
+    <h1 className="heading">리액트 시작!</h1>
+    <p className="mainTitle">타이틀 영역입니다.</p>
+    <p className="contents">본문 영역입니다.</p>
+    </>
+  )
+}
+export default App;
---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+import "../src/Header.css";
+import Footer from "./Footer";
+function Header() {
+  return (
+    <>
+      <h2 className="subTitle">여기는 상단 영역입니다.</h2>
+      <Footer></Footer>
+    </>
+  );
+}
+export default Header;
